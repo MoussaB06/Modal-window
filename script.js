a6f102c3fe9fb87ba577ca5b8fbf04f79e20f9f9
MoussaB06 <mohamedmoussa.benmouhoub@se.univ-bejaia.dz>
2024-10-27 04:37:41 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+'use strict';
+let i;
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnClosedModal = document.querySelector('.close-modal');
+
+const btnOpenModal = document.querySelectorAll('.show-modal');
+// console.log(btnOpenModal);
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener('click', openModal);
+}
+
+const closedModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnClosedModal.addEventListener('click', closedModal);
+
+overlay.addEventListener('click', closedModal);
